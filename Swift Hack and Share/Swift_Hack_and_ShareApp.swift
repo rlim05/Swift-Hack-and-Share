f4d803f24471574516e0f4f68bc5a75db03425ec
Ryan Lim <rlim@bsd220.org>
2021-11-29 10:16:08 -0600

    Initial Commit

---
+++ Swift Hack and Share/Swift_Hack_and_ShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Hack_and_ShareApp.swift
+//  Swift Hack and Share
+//
+//  Created by Student on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Hack_and_ShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
