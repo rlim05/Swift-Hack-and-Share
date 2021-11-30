a8a117a8c2ecfb154e1dd96a8f1aad006a0c0a0a
Ryan Lim <rlim@bsd220.org>
2021-11-30 10:39:10 -0600

    Finished Date and Time Picker

--- Swift Hack and Share/ContentView.swift
+++ Swift Hack and Share/ContentView.swift
@@ -4,19 +4,35 @@
 //
 //  Created by Ryan Lim on 11/29/21.
 //
+//  To do list
+//  *Create a date picker
+//  *Create a scroller
 
 import SwiftUI
 
 struct ContentView: View {
     @State private var color = Color.white
+    @State private var date = Date()
     var body: some View {
         VStack {
             Text("Pickers")
                 .font(.title)
                 .fontWeight(.bold)
                 .padding()
-            ColorPicker("Color Picker", selection: $color)
-                .frame(width: 150, height: 150, alignment: .center)
+            Text("Color Picker")
+                .font(.title)
+            ColorPicker("Choose Your Favorite Color", selection: $color)
+                .frame(width: 150, height: 75, alignment: .center)
+            Text("Date Picker")
+                .font(.title)
+            HStack {
+                Text("Choose Today's Date and Time")
+                    .padding()
+            }
+            DatePicker("", selection: $date)
+                .frame(width: 150, height: 25, alignment: .center)
+                .padding()
+            Text("Today's Date is \(date)")
         }
     }
 }
